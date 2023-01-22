a59693546a7692112c9fa14250c7d5b98d6b9fc6
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-01-22 18:01:42 +0100

    add date modification on controller

--- src/controllers/admin/m_articles_admin.php
+++ src/controllers/admin/m_articles_admin.php
@@ -14,19 +14,19 @@ if(isset($_POST['id'] ,$_POST)) {
     $get_id = htmlspecialchars($_POST['id']);
     if (isset($_POST['titre']) and !empty($_POST['titre']) and $_POST['titre'] != $post['titre']) {
         $newtitle = htmlspecialchars($_POST['titre']);
-        $inserttitle = $database->prepare("UPDATE posts SET title = ? WHERE id = ?");
+        $inserttitle = $database->prepare("UPDATE posts SET title = ?, date_post = NOW() WHERE id = ?");
         $inserttitle->execute(array($newtitle, $get_id));
         header('Location: http://localhost/mon-blog/templates/admin_office/article-modifs.php?id=' . $get_id);
     }
     if (isset($_POST['type']) and !empty($_POST['type']) and $_POST['type'] != $post['type']) {
         $newtype = htmlspecialchars($_POST['type']);
-        $inserttype = $database->prepare("UPDATE posts SET work_type = ? WHERE id = ?");
+        $inserttype = $database->prepare("UPDATE posts SET work_type = ?, date_post = NOW() WHERE id = ?");
         $inserttype->execute(array($newtype, $get_id));
         header('Location: http://localhost/mon-blog/templates/admin_office/article-modifs.php?id=' . $get_id);
     }
     if (isset($_POST['contenu']) and !empty($_POST['contenu']) and $_POST['contenu'] != $post['contenu']) {
         $newcontent = htmlspecialchars($_POST['contenu']);
-        $insertcontent = $database->prepare("UPDATE posts SET content = ? WHERE id = ?");
+        $insertcontent = $database->prepare("UPDATE posts SET content = ?, date_post = NOW() WHERE id = ?");
         $insertcontent->execute(array($newcontent, $get_id));
         header('Location: http://localhost/mon-blog/templates/admin_office/article-modifs.php?id=' . $get_id);
     }
